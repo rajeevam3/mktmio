d3eb4b14393e280a5b2dbdb5b3d9e200ef4d7136
rajeevam3 <rajeevam3@gmail.com>
2022-06-30 04:41:12 -0400

    Style changes and footer correction

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -8,7 +8,8 @@ const Footer = () => (
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Bengaluru, Karnataka India <br /> All Rights Reserved</p>
+        <p>@2022 BNGLR Studios.</p>
+        {/* <p>Bengaluru, Karnataka India <br /> All Rights Reserved</p> */}
       </div>
       {/* <div className="gpt3__footer-links_div">
         <h4>Links</h4>
@@ -24,16 +25,16 @@ const Footer = () => (
         <p>Contact</p>
       </div> */}
       <div className="gpt3__footer-links_div">
-        {/* <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p> */}
+        <h4>Get in touch</h4>
+        {/* <p>Crechterwoord K12 182 DK Alknjkcb</p> */}
         <p>+91-8105646516</p>
         <p>gm@bnglr.io</p>
       </div>
     </div>
 
-    <div className="gpt3__footer-copyright">
-      <p>@2022 BNGLR Studios. All rights reserved.</p>
-    </div>
+    {/* <div className="gpt3__footer-copyright">
+      <p>@2022 BNGLR Studios.</p>
+    </div> */}
   </div>
 );
 
